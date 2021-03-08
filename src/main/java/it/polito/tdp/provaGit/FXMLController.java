04b4e8918337192dbedac4b442c12aa8ad296cea
beatr <beatr@LAPTOP-P4TKASTV.station>
2021-03-08 09:35:06 +0100

    Modifica sciocca

--- src/main/java/it/polito/tdp/provaGit/FXMLController.java
+++ src/main/java/it/polito/tdp/provaGit/FXMLController.java
@@ -4,22 +4,32 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-public class FXMLController implements Initializable {
-    
+public class FXMLController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
     @FXML
     private Label label;
-    
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private Button bottone;
+
+    @FXML
+    void handleButtonAction(ActionEvent event) {
+    	System.out.println("Bottone premuto");
     }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+
+    @FXML
+    void initialize() {
+        assert label != null : "fx:id=\"label\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert bottone != null : "fx:id=\"bottone\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
 }
